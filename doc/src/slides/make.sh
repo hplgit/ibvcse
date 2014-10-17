e04ff0647c0e0024091dffd48c0ff8172da0fa4c
Hans Petter Langtangen <hpl@simula.no>
2014-10-17 13:48:48 +0200

    updates

--- doc/src/slides/make.sh
+++ doc/src/slides/make.sh
@@ -4,6 +4,8 @@ name=csebio
 doconce format html $name --html_style=solarized3
 doconce slides_html $name doconce --nav_button=text
 
+doconce format ipynb $name
+
 # Publish
 dest=../../pub/slides
 cp $name.html ._${name}*.html $dest
